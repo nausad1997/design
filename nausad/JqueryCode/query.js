b7fe133e875df78c2eb1ead32da5a93254ee6314
nausad <nausad1886@gmail.com>
2019-09-10 00:13:43 +0530

    k

---
+++ nausad/JqueryCode/query.js
@@ -0,0 +1,59 @@
+  
+
+  //1
+
+  // $(document).ready(()=>
+  //   {
+
+  //      $("#click").click(()=>{
+
+  //       	$(".paragraph").addClass("fig-font");
+
+  //      }); 
+       
+  //      $("#newElement").dblclick(()=>{
+
+  //      		$(".paragraph").css("color","red");
+  //      });
+
+  //      $(".my-image").hover(()=>{
+
+  //      		$("#something").hide(5000);
+  //      });
+     
+       
+        
+  //  });//end of doc function
+
+
+
+//2
+
+ //  $(document).ready(()=>{
+
+ //  	$("#email-input").trigger("select");
+
+
+ //  	$("input").focus(function(){
+
+ //  		$(this).addClass("some-color");
+
+ //  	});
+
+ //  	$("input").focusout(function(){
+
+ //  		$(this).removeClass("some-color");
+
+ //  	});
+
+ //  	$("form").submit((event)=>{
+
+ //  		event.preventDefault();
+ //  		alert("user logged-in sucessfully");
+
+ //   });
+
+ // });
+
+
+
